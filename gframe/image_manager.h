98d641d1fd15b47db4b244bb9749484d4db97d88
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-04-19 18:18:18 +0800

    Add files via upload

---
+++ gframe/image_manager.h
@@ -0,0 +1,56 @@
+#ifndef IMAGEMANAGER_H
+#define IMAGEMANAGER_H
+
+#include "config.h"
+#include "data_manager.h"
+#include <unordered_map>
+
+namespace ygo {
+
+class ImageManager {
+public:
+	bool Initial();
+	void SetDevice(irr::IrrlichtDevice* dev);
+	void ClearTexture();
+	void RemoveTexture(int code);
+	irr::video::ITexture* GetTextureFromFile(char* file, s32 width, s32 height);
+	irr::video::ITexture* GetTexture(int code, bool fit = false);
+	irr::video::ITexture* GetTextureThumb(int code);
+	irr::video::ITexture* GetTextureField(int code);
+
+	std::unordered_map<int, irr::video::ITexture*> tMap[2];
+	std::unordered_map<int, irr::video::ITexture*> tThumb;
+	std::unordered_map<int, irr::video::ITexture*> tFields;
+	irr::IrrlichtDevice* device;
+	irr::video::IVideoDriver* driver;
+	irr::video::ITexture* tCover[2];
+	irr::video::ITexture* tUnknown;
+	irr::video::ITexture* tAct;
+	irr::video::ITexture* tAttack;
+	irr::video::ITexture* tNegated;
+	irr::video::ITexture* tChain;
+	irr::video::ITexture* tNumber;
+	irr::video::ITexture* tLPFrame;
+	irr::video::ITexture* tLPBar;
+	irr::video::ITexture* tMask;
+	irr::video::ITexture* tEquip;
+	irr::video::ITexture* tTarget;
+	irr::video::ITexture* tChainTarget;
+	irr::video::ITexture* tLim;
+	irr::video::ITexture* tOT;
+	irr::video::ITexture* tHand[3];
+	irr::video::ITexture* tBackGround;
+	irr::video::ITexture* tBackGround_menu;
+	irr::video::ITexture* tBackGround_deck;
+	irr::video::ITexture* tField[2];
+	irr::video::ITexture* tFieldTransparent[2];
+	irr::video::ITexture* tRScale[14];
+	irr::video::ITexture* tLScale[14];
+	std::list<std::string> support_types;
+};
+
+extern ImageManager imageManager;
+
+}
+
+#endif // IMAGEMANAGER_H
