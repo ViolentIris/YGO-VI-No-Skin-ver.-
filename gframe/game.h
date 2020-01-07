0331cda8f80dcb24dfb47d73f92d6455f32cd995
VI-1911 <2571845421@qq.com>
2020-01-07 23:30:12 +0800

    .

--- gframe/game.h
+++ gframe/game.h
@@ -35,6 +35,7 @@ struct Config {
 	int chkWaitChain;
 	int chkIgnore1;
 	int chkIgnore2;
+	int default_rule;
 	int chkHideSetname;
 	int chkHideHintButton;
 	int control_mode;
@@ -105,6 +106,7 @@ struct BotInfo {
 	wchar_t desc[256];
 	bool support_master_rule_3;
 	bool support_new_master_rule;
+	bool support_master_rule_2020;
 };
 
 struct FadingUnit {
@@ -456,7 +458,7 @@ public:
 	irr::gui::IGUIStaticText* stBotInfo;
 	irr::gui::IGUIButton* btnStartBot;
 	irr::gui::IGUIButton* btnBotCancel;
-	irr::gui::IGUICheckBox* chkBotOldRule;
+	irr::gui::IGUIComboBox* cbBotRule;
 	irr::gui::IGUICheckBox* chkBotHand;
 	irr::gui::IGUICheckBox* chkBotNoCheckDeck;
 	irr::gui::IGUICheckBox* chkBotNoShuffleDeck;
@@ -784,7 +786,7 @@ extern Game* mainGame;
 #define BUTTON_REPLAY_CANCEL		331
 #define BUTTON_BOT_START			340
 #define LISTBOX_BOT_LIST			341
-#define CHECKBOX_BOT_OLD_RULE		342
+#define COMBOBOX_BOT_RULE			342
 #define LISTBOX_SINGLEPLAY_LIST		343
 #define BUTTON_LOAD_SINGLEPLAY		344
 #define BUTTON_CANCEL_SINGLEPLAY	345
@@ -888,7 +890,7 @@ extern Game* mainGame;
 #define TEXTURE_ACTIVATE			8
 #define TEXTURE_HEAD_S				9
 
-#define DEFAULT_DUEL_RULE			4
+#define DEFAULT_DUEL_RULE			5
 
 #define CARD_ARTWORK_VERSIONS_OFFSET	10
 
